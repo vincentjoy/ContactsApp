0663db202592c8e7d1b03a4741a26949cd997c08
Vincent Joy <joy.vincent@rehlat.com>
2019-02-04 22:05:20 +0530

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Vincent Joy on 04/02/19.
+//  Copyright © 2019 Contacts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
