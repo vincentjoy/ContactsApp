a29936f3f17fdf545838d24343c361458f11d7f3
Vincent Joy <joy.vincent@rehlat.com>
2019-02-07 13:50:59 +0530

    Profile photo uploading in add contacts screen

--- ContactsApp/Controllers/AddEditContacts/AddEditContactsTableViewController.swift
+++ ContactsApp/Controllers/AddEditContacts/AddEditContactsTableViewController.swift
@@ -25,4 +25,44 @@ class AddEditContactsTableViewController: UITableViewController {
     @objc func cancelAction() {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    @IBAction func addPhoto(_ sender: UIButton) {
+        
+        let actionSheet = UIAlertController(title: "Select an image from", message: "", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) -> Void in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                let myPickerController = UIImagePickerController()
+                myPickerController.delegate = self
+                myPickerController.sourceType = .camera
+                self.present(myPickerController, animated: true, completion: nil)
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { (action) -> Void in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                let myPickerController = UIImagePickerController()
+                myPickerController.delegate = self
+                myPickerController.sourceType = .photoLibrary
+                self.present(myPickerController, animated: true, completion: nil)
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
 }
+
+extension AddEditContactsTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let chosenImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            outletObject.profilePicture.image = chosenImage
+        }
+    }
+}
+
